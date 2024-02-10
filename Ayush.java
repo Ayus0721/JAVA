4d4f018936d26d3609c0e2c55e6d711d0c98bf76
AYUSH <147092631+Ayus0721@users.noreply.github.com>
2024-02-10 20:19:00 +0530

    Add files via upload

---
+++ Ayush.java
@@ -0,0 +1,8 @@
+class Ayush{
+    public static void main (String args[]){
+        System.out.println("Hello world ");
+       System.out.println("Hi im trying something new in java");
+        System.out.println("Hi im trying something new in java");
+    }
+}
+ 
