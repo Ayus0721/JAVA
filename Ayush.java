eb6c5efefb4e9b572bb50fe7696efdbe096e54d8
AYUSH <147092631+Ayus0721@users.noreply.github.com>
2024-02-21 11:01:37 +0530

    Add files via upload

---
+++ Ayush.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Ayush{
+    public static void main (String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+
+            String name = sc.nextLine();
+            System.out.println(name);
+        
+        
+    }
+}
+ 
