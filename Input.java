886b0295a2ca7b49aaf539d01b94f202315c5c24
AYUSH <147092631+Ayus0721@users.noreply.github.com>
2024-02-21 11:03:06 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class Input {
+
+
+    public static void main (String args[]){
+        System.out.println("Give any two number");
+        Scanner sc = new Scanner(System.in);
+
+            System.out.print("a=");
+            int a = sc.nextInt();
+            System.out.print("b=");
+            int b = sc.nextInt();
+            System.out.println(a+b); 
+        
+    }
+}
