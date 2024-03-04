82c02a33dcab9248ec6d789dd0e23be8036c4df1
AYUSH <147092631+Ayus0721@users.noreply.github.com>
2024-03-04 21:49:25 +0530

    Add files via upload

---
+++ ForLoops.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ForLoops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++){
+            System.out.print(i+ " ");
+            i++;
+
+        }
+    }
+}
