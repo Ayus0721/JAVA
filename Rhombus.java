3fd70f4cb9c27769612311422f481eb8c6dc99e3
AYUSH <147092631+Ayus0721@users.noreply.github.com>
2024-03-11 19:41:10 +0530

    Add files via upload

--- Rhombus.java
+++ Rhombus.java
@@ -8,14 +8,16 @@ public class Rhombus {
         System.out.println("Enter the number of rows");
         int row = sc.nextInt();
         
-        //upper half pattern
+        //outer loop
         for(int i= 1 ; i<= row; i++){
-            //1st part
+            //inner loop
+            //spaces
+            
             for(int j=row-i; j>=1; j--){
                 System.out.print(" ");
             }
+            //stars
 
-            
             for(int j= 1; j<=row;j++){
                 System.out.print("*");
             }
